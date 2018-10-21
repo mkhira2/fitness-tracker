dab00b42f3febcb67bb78d139a4c75927cf88341
Kenji Hirabayashi <mkhira2@gmail.com>
2018-10-21 15:54:58 -0500

    stores completed exercises on firestore

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -42,7 +42,7 @@ export class TrainingService {
     }
 
     completeExercise() {
-        this.exercises.push({
+        this.addDataToDatabase({
             ...this.runningExercise, 
             date: new Date(), 
             state: 'completed'
@@ -52,7 +52,7 @@ export class TrainingService {
     }
 
     cancelExercise(progress: number) {
-        this.exercises.push({
+        this.addDataToDatabase({
             ...this.runningExercise, 
             duration: this.runningExercise.duration * (progress / 100),
             calories: this.runningExercise.calories * (progress / 100),
@@ -69,5 +69,9 @@ export class TrainingService {
 
     getCompletedOrCancelledExercises() {
         return this.exercises.slice();
+    }
+
+    private addDataToDatabase(exercise: Exercise){
+        this.db.collection('finishedExercises').add(exercise);
     }
 }
